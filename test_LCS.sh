077d16c34421121700761d013c1f362e5724e27b
drberg1000 <drberg1000@gmail.com>
2014-12-16 01:54:29 -0600

    Added test cases.  Added Test Script.

--- test_LCS.sh
+++ test_LCS.sh
@@ -1,31 +1,34 @@
 #String Description
-gcc LCS_serial.c -o LCS_serial
+#gcc LCS_serial.c     -o LCS_serial
+gcc -fopenmp LCS_omp_diagonal.c -o LCS_omp_diagonal
+
 N=(  Tiny\ Strings\ from\ CLRS
      Long\ Strings\ from\ CLRS
   )
 
 ##String 1
-S1=(  ABCBDAB
-     ACCGGTCGAGTGCGCGGAAGCCGGCCGAA
+S1=( tests/micro_test1.txt
+     tests/tiny_test1.txt
   )
 
 #String 2
-S2=( BDCABA
-    GTCGTTCGGAATGCCGTTGCTCTGTAAA
+S2=( tests/micro_test2.txt
+     tests/tiny_test2.txt
   )
 
 #Longest Common Substring
-S=( BCBA
-    GTCGTCGGAAGCCGGCCGAA
+S=( tests/micro_test_LCS.txt
+    tests/tiny_test_LCS.txt
   )
 
 
 
 for ((i=0; i < ${#S1[@]}; i++));
 do
-   RESULT=$(./LCS_serial ${S1[$i]} ${S2[$i]} | head -1 )
+#   RESULT=$(./LCS_serial ${S1[$i]} ${S2[$i]} | head -1 )
+   RESULT=$(./LCS_omp_diagonal 2 ${S1[$i]} ${S2[$i]} | head -1 )
 
-   if [ "$RESULT" = "${S[$i]}" ] 
+   if [ "$RESULT" = `cat "${S[$i]}"` ] 
    then
      success="PASSED" 
    else
